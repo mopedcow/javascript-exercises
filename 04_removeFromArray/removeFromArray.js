d7f19deb713293ef0ba2832813bb03b53aef190d
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-03-11 21:25:15 +0000

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray, ...itemsToRemove) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+        console.log(`At argument position: ${j}:`);
+        for (let i = 0; i < userArray.length; i++) {
+            console.log(`userArray at ${i}: ${userArray[i]}, arg number: ${itemsToRemove[j]}.`);
+            if (userArray[i] === itemsToRemove[j]) {
+                console.log(`Found '${userArray[i]}' at ${i}`);
+                userArray.splice(i, 1);
+                i = 0;
+                console.log(`user array: ` + userArray);
+            }
+        }
+    }
+    
+    return userArray;
 };
 
 // Do not edit below this line
