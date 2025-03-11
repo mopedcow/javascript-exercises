dbdc9da3fea0a59dfcc6525db770c5bd0e396265
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-03-11 22:36:53 +0000

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    if (startNum < 0 || endNum < 0) return 'ERROR';
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return 'ERROR';
+    //my solution for sorting args high to low (before checking TOP solution):
+    //  let numArray = [startNum, endNum];
+    //  numArray.sort();
+    if (startNum > endNum) [startNum, endNum] = [endNum, startNum];
+    //console.log(startNum + ", " + endNum);
+    let finalSum = 0;
+    for (let i = startNum; i <= endNum; i++){
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
