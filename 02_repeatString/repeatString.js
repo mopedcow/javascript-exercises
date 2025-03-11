7cc569ab35c455e1310356d4bbce11d72866f451
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-03-11 13:54:41 +0000

    Complete first 2 excersises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatString = "";
+    for (num; num > 0; num--) {
+        repeatString += string;
+    }
+    if (num < 0) {
+        return 'ERROR';
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
