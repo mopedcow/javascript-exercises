2d3239cd9706bd6926ad7411c0083b550e67f6a4
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-03-11 14:28:25 +0000

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    //convert string to an array
+    stringToArray = stringToReverse.split("");
 
+    //reverse the order of the array
+    reversedArray = stringToArray.toReversed();
+
+    //transform the reversed array to a string (used join() instead of toString())
+    reversedString = reversedArray.join('');
+
+    return reversedString;
 };
 
 // Do not edit below this line
