0d92460367005a6160d20435d9f73061a530d112
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-03-12 15:29:10 +0000

    Comeplete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  tempToCelsius = (temp - 32) * 0.5555555556;
+  tempToCelsius = tempToCelsius.toFixed(1);
+  tempToCelsius = Number(tempToCelsius);
+  return tempToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  tempToFarenheit = temp * 1.8 + 32;
+  tempToFarenheit = tempToFarenheit.toFixed(1);
+  tempToFarenheit = Number(tempToFarenheit);
+  return tempToFarenheit;
 };
 
 // Do not edit below this line
